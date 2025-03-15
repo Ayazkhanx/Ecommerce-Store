d1e4b7e45c8bd04d9234c9e7964feedf85c807cf
Ayazkhanx <143736083+Ayazkhanx@users.noreply.github.com>
2025-03-15 21:10:21 +0500

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,18 +1,37 @@
-import express from 'express';
+const express = require('express')
 const app = express();
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const cokieeParser = require('cookie-parser')
+const cors = require('cors')
 
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
+MongoUrl = 'mongodb+srv://ak96979590:wcSGu9b8o1uQ3E0T@shoppingapp.c4whx.mongodb.net/'
 
-const connectToDb = (req, res) => {
-    mongoose.connect('mongodb://localhost:27017/mydatabase')
+const connectToDb = () => {
+    mongoose.connect(MongoUrl)
        .then(() => console.log('MongoDB Connected...'))
        .catch(err => console.log(err));
-
 }
 connectToDb()
+
+app.use(express.json())
+app.use(cokieeParser())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    method: ['POST', 'PUT', 'DELETE', 'GET'],
+    allowHeaders: [
+        'Content-type', 
+        'Authorization',
+        'Cache-control',
+        'Expires',
+        'Pragma',
+    ],
+    credentials: true
+    
+}))
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
